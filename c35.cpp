219243f20767fc490ec2d3fd1c98cf562e4c7d9b
Rehan Ali maan <130174371+RehanAliMaan@users.noreply.github.com>
2024-01-09 17:24:00 +0500

    Add files via upload

---
+++ c35.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int arr[5],i,sum=0;
+	float avg=0.0;
+	for(i=0;i<5;i++)
+	{
+		cout<<"enter the value";
+		cin>>arr[i];
+		sum=sum+arr[1];
+	}
+	avg=sum/5;
+	cout<<"the sum is"<<sum<<endl;
+	cout<<"the average is"<<avg<<endl;
+	return 0;
+	
+	
+}
